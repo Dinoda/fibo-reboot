170124b870aee97d0a74ddad7eed2ee693a49807
Antoine Motot <antoine.motot@gmail.com>
2026-02-24 15:26:48 +0000

    Fixed a non-awaited promise bug.

--- packages/migrations/src/migrate.js
+++ packages/migrations/src/migrate.js
@@ -3,5 +3,7 @@ import { Database } from 'fibo-database';
 import getFileContent from './fs/getFileContent.js';
 
 export default async (database, file) => {
-	await database.query(getFileContent(file));
+	const content = await getFileContent(file);
+
+	await database.query(content, []);
 }
